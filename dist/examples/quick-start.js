2e831527b8c2cdb9d5e754f427bbdf154af89c56
cris.wang.2000 <cris.wang.2000>
2022-04-21 10:13:08 -0400

    * fix some erorrs

---
+++ dist/examples/quick-start.js
@@ -0,0 +1,12 @@
+import { ProxyServer } from '../index';
+const server = new ProxyServer({
+    upstreamUrl: "http://localhost:3000",
+    port: 8080
+});
+// now when you call http://localhost:8080/new-posts
+// it will redirect the request to http://localhost:3000/posts
+server.proxy("posts")
+    .renameTo("new-posts");
+// server running
+server.serve();
+//# sourceMappingURL=quick-start.js.map
